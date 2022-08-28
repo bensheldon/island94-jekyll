da41f8410549f9911fa81b5665b190b9b769ab6e
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-08-27 17:39:17 -0700

    Add on-site search with Lunr.js

---
+++ assets/javascripts/search.js
@@ -0,0 +1,59 @@
+---
+---
+{% include_relative vendor/lunr.min.js %}
+
+(function() {
+  const RESULT_SIZE = 500
+
+  function formatSearchResults(results, store) {
+    if (results.length) {
+      let output = "";
+
+      for (const result of results) {
+        const item = store[result.ref];
+        output += '<a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+        output += '<p>' + item.content.substring(0, RESULT_SIZE) + '...</p>';
+      }
+
+      return output;
+    } else {
+      return '<p>No results found</p>';
+    }
+  }
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const searchTerm = urlParams.get("q");
+  const resultsEl = document.getElementById('search-results');
+
+  if (!searchTerm) {
+    resultsEl.innerHTML = '<p>No search term</p>';
+    return
+  }
+  document.getElementById('search-box').setAttribute("value", searchTerm);
+
+
+  fetch('/search.json').then(function (response) {
+    return response.json();
+  }).then(function (items) {
+    const idx = lunr(function() {
+      this.field('id');
+      this.field('title', { boost: 10 });
+      this.field('tags', { boost: 10 });
+      this.field('content');
+
+      for (const key in items) {
+        const item = items[key];
+
+        this.add({
+          'id': key,
+          'title': item.title,
+          'tags': item.tags,
+          'content': item.content
+        });
+      }
+    });
+
+    const searchResults = idx.search(searchTerm);
+    resultsEl.innerHTML = formatSearchResults(searchResults, items);
+  });
+})();
