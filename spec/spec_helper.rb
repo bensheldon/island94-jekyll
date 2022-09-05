d980bf855dfbce58c939da5d142f2dd7fdc24045
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-09-05 13:59:48 -0700

    Update rack-jekyll and rack

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,6 @@ require 'rack/jekyll'
 
 # https://stackoverflow.com/a/52507221/241735
 jekyll = Rack::Jekyll.new(force_build: ENV.fetch('JEKYLL_FORCE_BUILD', false), auto: true)
-sleep 0.1 while jekyll.compiling?
-
 Capybara.app = jekyll
 RSpec.configure do |config|
   config.include Capybara::DSL
