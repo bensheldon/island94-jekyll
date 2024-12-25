72f84ab3624a16be5f94ce5a7ec94e8323cec6d9
bensheldon <bensheldon@users.noreply.github.com>
2024-12-25 19:35:53 +0000

    deploy: c4c60caaef005571af1dc1d61aaee7809eb59251

--- app/helpers/markdown_helper.rb
+++
@@ -1,5 +0,0 @@
-module MarkdownHelper
-  def markdownify(text)
-    Kramdown::Document.new(text, input: 'GFM').to_html.html_safe
-  end
-end
--- app/models/application_model.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationModel
-  include ActiveModel::Model
-end
-require 'front_matter_parser'
-
